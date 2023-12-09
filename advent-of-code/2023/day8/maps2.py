b5cd54f01eb5d5d61a6c8767b067cbfdf7f70998
Karim Aouini <karim.aouini@docc.techstarter.de>
2023-12-09 12:26:43 +0100

    AoC23 Day 8 Part 2 Done

--- advent-of-code/2023/day8/maps2.py
+++ advent-of-code/2023/day8/maps2.py
@@ -27,25 +27,52 @@ for i in range(2, len(data)):
     nodes.append(node.strip())
     references.append(refs)
 
-noEnd = True
-steps = 0
+# tries = 12737
+tries = 1
 snodes = list(filter(lambda s: s[-1] == "A", nodes))
-
-while noEnd:
-    for d in directions:
-        for i, node in enumerate(snodes):
-            inode = nodes.index(node)
+pathlengths = []
+diffs = []
+for i, node in enumerate(snodes):
+    steps = 0
+    noEnd = True
+    pathlengths.append([])
+    while noEnd:
+        for d in directions:
+            inode = nodes.index(snodes[i])
             refs = references[inode]
             if d == 'L': snodes[i] = refs[0]
             if d == 'R': snodes[i] = refs[1]
-        steps += 1
+            steps += 1
+            if snodes[i].endswith("Z"):
+                pathlengths[i].append(steps)
+                if len(pathlengths[i]) > tries:
+                    noEnd = False
+                    break
+    for n in range(len(pathlengths[i]) - 1):
+        diffs.append(pathlengths[i][n + 1] - pathlengths[i][n])
 
-        allEndZ = True
-        for node in snodes:
-            if not node.endswith("Z") : allEndZ = False
+noEnd = True
+div = 2
+divs = []
+while noEnd:
+    didDivide = False
+    for i, node in enumerate(diffs):
+        if node % div == 0: 
+            diffs[i] = int(node/div)
+            didDivide = True
+    if didDivide: divs.append(div)
 
-        if allEndZ:
-            noEnd = False
-            break
+    goOn = False
+    for d in range(len(diffs) - 1):
+        if diffs[d] != 1:
+            goOn = True
+    if goOn:
+        div += 1 
+    else:
+        noEnd = False
 
-print(steps)
+lcm = 1
+for i in range(len(divs)):
+    lcm = lcm * divs[i]
+
+print(lcm)
