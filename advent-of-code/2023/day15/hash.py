b134556937186d0154a7a6932485d781d70d214f
Karim Aouini <karim.aouini@docc.techstarter.de>
2023-12-15 15:56:18 +0100

    AoC23 Day 15 Part1 & Part2 Completed

--- advent-of-code/2023/day15/hash.py
+++ advent-of-code/2023/day15/hash.py
@@ -2,8 +2,8 @@ import os
 base = os.environ["AOC_BASE"]
 
 # Read File
-path = base + "/sample2"
-# path = base + "/input"
+# path = base + "/sample2"
+path = base + "/input"
 data = open(path).read().strip()
 data = data.split(",")
 sum1 = 0
@@ -32,27 +32,26 @@ for i in range(len(data)):
             if b[0][0] == box: 
                 boxNotInStack = False
                 # box exists, now check if same lensgroup exists
+                addNewLens= True
                 for bb in range(1, len(b)):
-                    isSame = True
                     if len(b[bb]) == len(data[i]):
-                        for ii in range(len(b[bb]) - 2):
-                            if b[bb][ii] != data[i][ii]:
-                                isSame = False
-                                break
-                        if isSame: boxes[boxes.index(b)][b.index(b[bb])] = data[i]
-                    if not isSame: boxes[boxes.index(b)].append(data[i])
+                        if b[bb][:b[bb].index("="):] == data[i][:data[i].index("="):]: 
+                            boxes[boxes.index(b)][b.index(b[bb])] = data[i]
+                            addNewLens = False
+                            break
+                if addNewLens: boxes[boxes.index(b)].append(data[i])
         if boxNotInStack: 
             boxes.append([[box], data[i]])
 
     except:
         for b in boxes: 
             if b[0][0] == box: 
-                for bb in b:
-                    if len(bb) - 1 == len(data[i]):
-                        isSame = True
-                        for ii in range(len(bb) - 2):
-                            if bb[ii] != data[i][ii]:
-                                isSame = False
-                                break
-                        if isSame: b.pop(b.index(bb))
-print(str(sum1))
+                for bb in range(1, len(b)):
+                    if b[bb][:b[bb].index("="):] == data[i][:data[i].index("-"):]: 
+                        b.pop(b.index(b[bb]))
+                        break
+sum2 = 0
+for b in boxes:
+    for bb in range(1, len(b)):
+        sum2 += (b[0][0] + 1) * bb * int(b[bb][-1])
+print(str(sum2))
